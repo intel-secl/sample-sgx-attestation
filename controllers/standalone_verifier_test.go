af3efb15c02a57e9a4a4023adce48f0085961210
Ullas, Manu <manu.ullas@intel.com>
2020-11-04 16:12:18 +0530

    Add root CA pem for unit tests

--- controllers/standalone_verifier_test.go
+++ controllers/standalone_verifier_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestStandaloneVerifier_VerifyQuote(t *testing.T) {
-	sv := StandaloneVerifier{Config: &config.Configuration{}}
+	sv := StandaloneVerifier{Config: &config.Configuration{TrustedRootCAPath: "../test/root_ca.pem"}}
 	quoteRaw, _ := ioutil.ReadFile("../pkg/tenantapp/resources/quote.dat")
 	// we need to convert to base64 before sending to verifier
 	assert.NoError(t, sv.VerifyQuote(base64.StdEncoding.EncodeToString(quoteRaw)))
