d761208ea6db162c145f37c211b54e9e9d2b4863
Ullas, Manu <manu.ullas@intel.com>
2020-11-09 02:50:17 +0530

    Remove inner loop from handleConnection

--- pkg/tenantapp/server.go
+++ pkg/tenantapp/server.go
@@ -29,33 +29,31 @@ func (a *TenantServiceApp) handleConnection(c net.Conn) {
 	defer c.Close()
 
 	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-	for {
-		rawData, err := bufio.NewReader(c).ReadBytes('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		sh := controller.SocketHandler{Config: a.Config}
-
-		taRequest := controllers.UnmarshalRequest(rawData)
-
-		switch taRequest.RequestType {
-		case constants.ReqTypeConnect:
-			resp, err = sh.HandleConnect(taRequest)
-		case constants.ReqTypePubkeyWrappedSWK:
-			resp, err = sh.HandlePubkeyWrappedSWK(taRequest)
-		case constants.ReqTypeSWKWrappedSecret:
-			resp, err = sh.HandleSWKWrappedSecret(taRequest)
-			break
-		}
-
-		if err != nil {
-			defaultLog.WithError(err).Error("server:handleConnection Error processing request")
-		}
-		c.Write(controllers.MarshalResponse(*resp))
-		c.Write([]byte(constants.EndLine))
+	rawData, err := bufio.NewReader(c).ReadBytes('\n')
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	sh := controller.SocketHandler{Config: a.Config}
+
+	taRequest := controllers.UnmarshalRequest(rawData)
+
+	switch taRequest.RequestType {
+	case constants.ReqTypeConnect:
+		resp, err = sh.HandleConnect(taRequest)
+	case constants.ReqTypePubkeyWrappedSWK:
+		resp, err = sh.HandlePubkeyWrappedSWK(taRequest)
+	case constants.ReqTypeSWKWrappedSecret:
+		resp, err = sh.HandleSWKWrappedSecret(taRequest)
+		break
+	}
+
+	if err != nil {
+		defaultLog.WithError(err).Error("server:handleConnection Error processing request")
+	}
+	c.Write(controllers.MarshalResponse(*resp))
+	c.Write([]byte(constants.EndLine))
 }
 
 func (a *TenantServiceApp) StartServer() error {
