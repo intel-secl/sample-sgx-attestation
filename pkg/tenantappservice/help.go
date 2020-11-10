d52ed52ff3b1ffcc42e8fb8f4bbbb30ede7f5ccd
Ullas, Manu <manu.ullas@intel.com>
2020-11-10 19:26:07 +0530

    Fix usage info for tenant-app-service

--- pkg/tenantappservice/help.go
+++ pkg/tenantappservice/help.go
@@ -6,33 +6,28 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/intel-secl/sample-sgx-attestation/v3/pkg/tenantverifier/version"
 )
 
 const helpStr = `Usage:
-	sgx-tenant-app <command> [arguments]
+	sgx-tenantapp-service <command> [arguments]
 	
 Available Commands:
 	help|-h|--help         Show this help message
-	version|-v|--version   Show the version of current sgx-tenant-app build
+	version|-v|--version   Show the version of current sgx-tenantapp-service build
 	setup <task>           Run setup task
-	start                  Start sgx-tenant-app
-	status                 Show the status of sgx-tenant-app
-	stop                   Stop sgx-tenant-app
-	uninstall [--purge]    Uninstall sgx-tenant-app	
+	start                  Start sgx-tenantapp-service
+	status                 Show the status of sgx-tenantapp-service
+	stop                   Stop sgx-tenantapp-service
+	uninstall [--purge]    Uninstall sgx-tenantapp-service	
 
-Usage of sgx-tenant-app setup:
-	sgx-tenant-app setup <task> [--help] [--force] [-f <answer-file>]
+Usage of sgx-tenantapp-service setup:
+	sgx-tenantapp-service setup <task> [--help] [--force] [-f <answer-file>]
 		--help                      show help message for setup task
 		--force                     existing configuration will be overwritten if this flag is set
 
 Available Tasks for setup:
-	all                             Runs all setup tasks
-	server                          Setup http server on given port
-	download_ca_cert                Download CMS root CA certificate
-	download_cert tls               Download CA certificate from CMS for tls
-`
+	all                             Runs all setup tasks`
 
 func (a *App) printUsage() {
 	fmt.Fprintln(a.consoleWriter(), helpStr)
